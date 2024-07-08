603284284f51ac7d419ba3109052b879dbc10e30
Alejandro S <alexsori35@gmail.com>
2024-07-08 22:37:43 +0200

    basic chat view

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const app = express();
 const server = createServer(app);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/chat-view', (req, res) => {
+  res.sendFile(join(__dirname, 'pages/chat-view/index.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
